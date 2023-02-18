75ee1f19969a3d707e04a6f889be16bb97e2769c
Yavuz Çakmak <yavuz.cakmak@affintybox.com>
2023-02-19 01:20:16 +0200

    Change

---
+++ Moor.Core/Base/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moor.Core.Base
+{
+    public abstract class BaseEntity
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("created_date")]
+        public DateTime CreatedDate { get; set; }
+
+        [Column("update_date")]
+        public DateTime UpdateDate { get; set; }
+
+        [Column("is_deleted")]
+        public bool IsDeleted { get; set; }
+
+        [Column("status")]
+        public int Status { get; set; }
+    }
+}
---
+++ Moor.Repository/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Moor.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Contact> Contact { get; set; }
+        //public DbSet<Faq> Faq { get; set; }
+        //public DbSet<Sidebar> Sidebar { get; set; }
+        //public DbSet<Meaning> Meaning { get; set; }
+        //public DbSet<Content> Content { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
