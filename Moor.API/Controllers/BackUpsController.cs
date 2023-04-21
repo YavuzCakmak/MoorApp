53a3d6be2cbcd4d8356dcf2f7da8feaab5601538
Yavuz Çakmak <cakmak.yvz@gmail.com>
2023-04-21 23:34:25 +0300

    BackUpLastVersion

--- Moor.API/Controllers/BackUpsController.cs
+++ Moor.API/Controllers/BackUpsController.cs
@@ -17,10 +17,8 @@ namespace Moor.API.Controllers
     {
         public BackUpsController()
         {
-            // Crontab ifadesini ayarlayın (örneğin, her sabah saat 4'te çalışacak şekilde ayarlanmıştır)
             var schedule = CrontabSchedule.Parse("0 4 * * *");
 
-            // Yeniden başlatma durumunda programın doğru çalışmasını sağlamak için sonraki yedeklemeleri planlı hale getirin
             var nextRun = schedule.GetNextOccurrence(DateTime.Now);
             var timer = new System.Threading.Timer(_ =>
             {
@@ -35,83 +33,10 @@ namespace Moor.API.Controllers
             BackupDatabase();
         }
 
-        static void BackupDatabase()
+        static async void BackupDatabase()
         {
-
-            string server = "127.0.0.1";
-            string database = "moor";
-            string user = "root";
-            string password = "MpuH8QHcFniDNQv";
-
-            string backupDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Backup");
-            string backupFileName = "VeritabaniYedekleri-" + DateTime.Now.ToString("yyyy-MM-dd") + ".sql";
-            string backupFilePath = Path.Combine(backupDirectory, backupFileName);
-
-            // Eğer yedekleme dizini yoksa oluşturun
-            if (!Directory.Exists(backupDirectory))
-            {
-                Directory.CreateDirectory(backupDirectory);
-            }
-
-            // mysqldump komutunu oluştur
-            ProcessStartInfo psi = new ProcessStartInfo();
-            psi.FileName = @"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump.exe";
-            psi.RedirectStandardInput = false;
-            psi.RedirectStandardOutput = true;
-            psi.Arguments = string.Format(@"-u{0} -p{1} -h{2} {3}", user, password, server, database);
-            psi.UseShellExecute = false;
-
-            // mysqldump komutunu çalıştır
-            Process process = Process.Start(psi);
-
-            //using (StreamWriter output = new StreamWriter(backupFilePath))
-            //{
-            //    output.Write(process.StandardOutput.ReadToEnd());
-            //}
-
-            // yedek dosyasını oluştur
-            using (StreamWriter output = new StreamWriter(backupFilePath, false, Encoding.Unicode))
-            {
-                output.Write(process.StandardOutput.ReadToEnd());
-            }
-
-            process.WaitForExit();
-
-            Console.WriteLine($"Database backup created at '{backupFilePath}'.");
-
-            // Veritabanı yedeğini göndermek istediğiniz e-posta adresleri
-            string fromEmail = "anka_yedek@hotmail.com";
-            string toEmail = "musaozmen58@icloud.com";
-
-            // E-posta konusu ve içeriği
-            string emailSubject = $"Günlük Veritabanı Yedeği -- {DateTime.Now.Date}";
-            string emailBody = "Merhaba,\n\nGünlük veritabanı yedeği ektedir.";
-
-            // E-posta mesajını oluştur
-            var message = new MimeKit.MimeMessage();
-            message.From.Add(new MimeKit.MailboxAddress("Backup Service", fromEmail));
-            message.To.Add(MailboxAddress.Parse(toEmail));
-            message.Subject = emailSubject;
-
-            var builder = new MimeKit.BodyBuilder();
-            builder.TextBody = emailBody;
-            builder.Attachments.Add(backupFilePath);
-
-            message.Body = builder.ToMessageBody();
-
-            // E-posta gönderme işlemini başlat
-            using (var client = new MailKit.Net.Smtp.SmtpClient())
-            {
-                client.CheckCertificateRevocation = false;
-                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
-                client.Connect("smtp.office365.com", 587, SecureSocketOptions.StartTls);
-                client.Authenticate(fromEmail, "morey1010");
-                client.Send(message);
-                client.Disconnect(true);
-            }
-
-            // Yedek dosyasını sil
-            System.IO.File.Delete(backupFilePath);
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://localhost:7019/api/Settings/TakeBackUp");
         }
     }
 }
---
+++ SettingsApi/Controllers/SettingsController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using NCrontab;
+using System.Diagnostics;
+using System.Text;
+
+namespace CommonApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+
+        public SettingsController()
+        {
+        }
+
+        [HttpGet("TakeBackUp")]
+        public void TakeBackUp()
+        {
+            TakeBackUpExecute();
+        }
+
+        static void TakeBackUpExecute()
+        {
+            string server = "127.0.0.1";
+            string database = "moor";
+            string user = "Admin";
+            string password = "3X1Dkbp9#0UaKc4Ee9pY";
+
+            string backupDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Backup");
+            string backupFileName = "VeritabaniYedekleri-" + DateTime.Now.ToString("yyyy-MM-dd") + ".sql";
+            string backupFilePath = Path.Combine(backupDirectory, backupFileName);
+
+            // Eğer yedekleme dizini yoksa oluşturun
+            if (!Directory.Exists(backupDirectory))
+            {
+                Directory.CreateDirectory(backupDirectory);
+            }
+
+            string constring = $"server={server};user={user};pwd={password};database={database};";
+            string file = backupFilePath;
+            using (MySqlConnection conn = new MySqlConnection(constring))
+            {
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    using (MySqlBackup mb = new MySqlBackup(cmd))
+                    {
+                        cmd.Connection = conn;
+                        conn.Open();
+                        mb.ExportToFile(file);
+                        conn.Close();
+                    }
+                }
+            }
+
+            Console.WriteLine($"Database backup created at '{backupFilePath}'.");
+
+            string fromEmail = "anka_yedek@hotmail.com";
+            string toEmail = "musaozmen58@icloud.com";
+
+            string emailSubject = $"Günlük Veritabanı Yedeği -- {DateTime.Now.Date}";
+            string emailBody = "Merhaba,\n\nGünlük veritabanı yedeği ektedir.";
+
+            var message = new MimeKit.MimeMessage();
+            message.From.Add(new MimeKit.MailboxAddress("Backup Service", fromEmail));
+            message.To.Add(MimeKit.MailboxAddress.Parse(toEmail));
+            message.Subject = emailSubject;
+
+            var builder = new MimeKit.BodyBuilder();
+            builder.TextBody = emailBody;
+            builder.Attachments.Add(backupFilePath);
+
+            message.Body = builder.ToMessageBody();
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.CheckCertificateRevocation = false;
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                client.Connect("smtp.office365.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                client.Authenticate(fromEmail, "morey1010");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+
+            System.IO.File.Delete(backupFilePath);
+        }
+    }
+}
